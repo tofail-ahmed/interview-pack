90a007e8f3750e1c16870511236cc855694b8c12
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-05-23 20:29:01 +0600

    lesson2 done

--- lesson2.js
+++ lesson2.js
@@ -12,9 +12,9 @@
 let array = [1, 2, 3, 4, 5];
 [array[0], array[4]] = [array[4], array[0]];
 // console.log(array); //* [ 5, 2, 3, 4, 1 ]
-let a=1;
-let b=2;
-[a,b]=[b,a]
+// let a=1;
+// let b=2;
+// [a,b]=[b,a]
 // console.log({a,b}) //* { a: 2, b: 1 }
 
 //! 2. copy to clipboard
@@ -47,6 +47,60 @@ function copyToClipBoard(str) {
 
 //! 4. get value as data type
 
-const element = document.querySelector("#number").valueAsNumber;
+// const element = document.querySelector("#number").valueAsNumber;
 
-console.log( element);
+// console.log( element);
+
+//! 5. remove duplicates from array
+
+const array1 = [1, 2, 2, 2, 3, 5, 6, 5];
+
+// console.log([...new Set(array1)]); //* [1, 2, 3, 5, 6]
+
+//* 6. compare two arrays by value
+const hasSameElements = (a, b) => {
+  return a.length === b.length && a.every((v, i) => v === b[i]);
+};
+
+// console.log(hasSameElements([1, 2], [1, 2]));  //* true
+// console.log(hasSameElements([1, 2,2], [1, 2]));  //*false
+// console.log(hasSameElements([1, 1], [1, 1,1])); //*false
+
+//! 7. shuffling array
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+fruits.sort(); //? sort alphabetically
+// console.log(fruits) //* [ 'Apple', 'Banana', 'Mango', 'Orange' ]
+
+fruits.reverse(); //? The reverse() method reverses the elements in an array:
+// console.log(fruits) //* [ 'Orange', 'Mango', 'Banana', 'Apple' ]
+
+const months = ["Jan", "Feb", "Mar", "Apr"];
+const sorted = months.toSorted();
+// console.log(sorted) //* [ 'Apr', 'Feb', 'Jan', 'Mar' ]
+//? note: toSorted() keep original array unchanged but sort() not...
+const numbers = [1, 2, 3, 4, 5, 6,2,9,8];
+// console.log(numbers.sort()) //* [1, 2, 2, 3, 4, 5, 6, 8, 9]
+
+// console.log(numbers.sort(() => Math.random() - 0.5)); 
+
+
+// 8. comma operator
+
+let x = 1;
+x = (x++, x);
+
+console.log(x);
+
+let y = (2, 3);
+console.log(y);
+
+let a = [[1, 2, 3, 4], [3, 4, 5], [5, 6], [7]];
+
+for (let i = 0, j = 3; i <= 3; i++, j--) {
+  // console.log("a[" + i + "][" + j + "] = " + a[i][j]); //* a[0][3] = 4
+                                                          //* a[1][2] = 5
+                                                          //* a[2][1] = 6
+                                                          //* a[3][0] = 7
+}
