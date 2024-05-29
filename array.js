838ff838b0fc9a4dc76d1db2a7330706244ffc10
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-05-29 23:55:38 +0600

    map.filter, every, some array method added

--- array.js
+++ array.js
@@ -20,3 +20,19 @@ const numbers=[1,2,3]
 
 // console.log(numbers.includes(1)) //* true
 // console.log(numbers.includes(4)) //* false
+
+// console.log(numbers.map((number)=>(number*number))) //* [ 1, 4, 9 ]
+
+// console.log([1,2,3,4,5,6,7,8,9].filter((number)=>(number%2===0))) //* [ 2, 4, 6, 8 ]
+
+const numbers2=[1,2,3,4,5,6,7,8,9]
+
+// console.log(numbers2.every((number)=>(number>5))) //* false
+// console.log(numbers2.every((number)=>(number>0))) //* true
+// console.log(numbers2.every((number)=>(number>2))) //* false
+
+
+console.log(numbers2.some((number)=>(number>5))) //*true
+console.log(numbers2.some((number)=>(number<2))) //*true
+console.log(numbers2.some((number)=>(number>10))) //*false
+console.log(numbers2.some((number)=>(number<0))) //*false
