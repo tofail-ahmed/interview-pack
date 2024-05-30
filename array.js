6f8acd0b0a8734ecf1479cc47dad13b5e485d81c
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-05-30 12:50:15 +0600

    flat, slice added

--- array.js
+++ array.js
@@ -41,3 +41,20 @@ const numbers2=[1,2,3,4,5,6,7,8,9]
 // console.log(numbers2.fill(10)) //* [10,10,10,10,10,10,10,10]
 // console.log(numbers2.fill(-10)) //* [-10,-10,-10,-10,-10,-10,-10,-10,-10]
 
+// console.log(numbers2.reduce((a,c)=>a+c,0)) //* 45
+
+// console.log(numbers2.concat([11,22])) //*    [1, 2, 3, 4,  5, 6, 7, 8, 9, 11,22]
+
+// console.log(numbers2.reverse()) //* [9, 8, 7, 6, 5, 4, 3, 2, 1]
+const numbers3=[2,8,4,9,2,3,11,99,29]
+// console.log(numbers3.sort()) //! [11, 2, 2, 29, 3, 4, 8, 9,99] //! not work...
+// console.log(numbers3.join('+')) //* 2+8+4+9+2+3+11+99+29
+
+const numbers4=[1,2,3,4,5,[2,3,4,[2,5,6]]]
+// console.log(numbers4.flat(Infinity)) //* [ 1, 2, 3, 4, 5, 2, 3, 4,  2, 5, 6  ]
+
+// console.log(numbers4.findIndex((number)=>number===2)) //* 1
+
+// console.log(numbers3.toString()) //* 2,8,4,9,2,3,11,99,29
+
+// console.log(numbers3.slice(3,7)) //* [ 9, 2, 3, 11 ]
