72533c05cd910ff0947e1b6186e5d482054c0073
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-06-15 00:26:15 +0600

    setter getter generics added

---
+++ setter_getter.ts
@@ -0,0 +1,50 @@
+// function printData(data: number) {
+//       console.log("data: ", data);
+//   }
+  
+  
+//   printData(2);
+
+
+
+// function printData(data: number | string | boolean) {
+//       console.log("data: ", data);
+//   }
+  
+  
+//   printData(2);
+//   printData("hello");
+//   printData(true);
+
+
+
+// function printData<T>(data: T) {
+//       console.log("data: ", data);
+//   }
+  
+  
+//   printData(2);
+//   printData("hello");
+//   printData(true);
+
+// function printData<T>(data: T) {
+//       console.log("data: ", data);
+//   }
+  
+  
+//   printData(2);
+//   printData("hello");
+//   printData(true);
+//   printData([1, 2, 3, 4, 5, 6]);
+//   printData([1, 2, 3, "hi"]);
+//   printData({ name: "Ram", rollNo: 1 });
+
+
+// function printData<X,Y>(data1: X, data2: Y) {
+//       console.log("Output is: ", data1, data2);
+//   }
+  
+  
+//   printData("Hello", "World");
+//   printData(123, ["Hi", 123]);
+
